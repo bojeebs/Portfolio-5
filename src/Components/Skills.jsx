91a0ea019545a50098c8eab45ca13413d26b322e
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-02-20 16:30:59 -0600

    icons

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,9 +1,11 @@
+
+
+
+
 const Skills = () => {
   return (
     <div className = "skills-div">
-      <h2>SKILLS GO HERE Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      </h2>
+      <img  />
     </div>
   )
 }
