fac4b326877674e04f2d36e5103143e66c576dd3
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-05-12 12:03:04 -0500

    changed readme

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,4 +1,5 @@
-import placeholderImage from '../images/placeholder.jpeg';
+import placeholderImage from '../images/catsproject.png'
+import placeholderImage2 from '../images/steamclone.png'
 
 
 
@@ -21,13 +22,17 @@ const Projects = () => {
       </div>
       <div className="container-wrapper">
         <div className="container">
-          <img src={placeholderImage} alt="Placeholder" className="proimage" />
+        <a href="https://catfinder-43i8.onrender.com" target="_blank" rel="noopener noreferrer">
+    <img src={placeholderImage} alt="Placeholder" className="proimage" />
+    </a>
           <div className="content">
-            <h1>Title</h1>
-            <h3>Body</h3>
-            <div className="buttons">
-              <button className="orange-button">Github</button>
-              <button className="orange-button">View Project</button>
+            <h1 className="projectone">Cats</h1>
+            <h3 className="projectone">Cats was my first react project and pulls in a 3rd party api with cat breeds, images,<br/> and other cat information.
+            Each cat is clickable and provides a small amount of detail <br/> on each cat and ranks their attributes. You can also cycle through each cat using <br/> 
+            the next and previous buttons instead of going back to the home page. </h3>
+            <div className="buttonsone">
+            <a href="https://github.com/bojeebs/Cats" target="_blank" rel="noopener noreferrer" className="orange-button no-underline">Github</a>
+            <a href="https://catfinder-43i8.onrender.com" target="_blank" rel="noopener noreferrer" className="orange-button">View Project</a>
             </div>
           </div>
         </div>
@@ -35,29 +40,31 @@ const Projects = () => {
 
       <div className="container" style={{ flexDirection: "row-reverse" }}>
         <div>
-          <img src={placeholderImage} alt="Placeholder" className="proimage" />
+        <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+          <img src={placeholderImage2} alt="Placeholder" className="proimage" />
+        </a>
         </div>
         <div className="content">
-          <h1>Title</h1>
-          <h3>Body</h3>
-          <div className="buttons">
+          <h1 className="projecttwo">Steam Clone</h1>
+          <h3 className="projecttwo">Body</h3>
+          <div className="buttonstwo">
             <button className="orange-button">Github</button>
             <button className="orange-button">View Project</button>
           </div>
         </div>
       </div>
 
-      <div className="container">
+      {/* <div className="container">
         <img src={placeholderImage} alt="Placeholder" className="proimage" />
         <div className="content">
           <h1>Title</h1>
           <h3>Body</h3>
-          <div className="buttons">
+          <div className="buttonsthree">
             <button className="orange-button">Github</button>
             <button className="orange-button">View Project</button>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
   
